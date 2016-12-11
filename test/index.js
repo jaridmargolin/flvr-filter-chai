7d026e35f230edc2b29ecfa8016d3d9804b5b12f
Jarid Margolin <jaridmargolin@gmail.com>
2016-12-11 11:11:52 -0800

    Init commit.

---
+++ test/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* -----------------------------------------------------------------------------
+ * dependencies
+ * ---------------------------------------------------------------------------*/
+
+// 3rd party
+const assert = require('chai').assert;
+
+// lib
+const filter = require('../lib/index.js');
+
+
+/* -----------------------------------------------------------------------------
+ * test
+ * ---------------------------------------------------------------------------*/
+
+describe('flvr-filter-chai', function () {
+
+  it('Should remove chai frames from stack trace.', function () {
+    const orig = Error.prepareStackTrace;
+
+    Error.prepareStackTrace = function (_, stack) { return stack };
+
+    try {
+      assert.isTrue(false);
+    } catch (e) {
+      assert.isFalse(filter(null, e.stack)[0].toString().includes('chai/lib'));
+    }
+
+    Error.prepareStackTrace = orig;
+  });
+
+});
