794b129ab2b1a0e321811bb8f55a4367b1925eaa
Jarid Margolin <jaridmargolin@gmail.com>
2016-12-15 17:03:39 -0500

    Use standard coding style.

--- test/index.js
+++ test/index.js
@@ -1,34 +1,32 @@
-'use strict';
+/* eslint-env mocha */
+'use strict'
 
 /* -----------------------------------------------------------------------------
  * dependencies
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 // 3rd party
-const assert = require('chai').assert;
+const assert = require('chai').assert
 
 // lib
-const filter = require('../lib/index.js');
-
+const filter = require('../lib/index.js')
 
 /* -----------------------------------------------------------------------------
  * test
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 describe('flvr-filter-chai', function () {
-
   it('Should remove chai frames from stack trace.', function () {
-    const orig = Error.prepareStackTrace;
+    const orig = Error.prepareStackTrace
 
-    Error.prepareStackTrace = function (_, stack) { return stack };
+    Error.prepareStackTrace = function (_, stack) { return stack }
 
     try {
-      assert.isTrue(false);
+      assert.isTrue(false)
     } catch (e) {
-      assert.isFalse(filter(null, e.stack)[0].toString().includes('chai/lib'));
+      assert.isFalse(filter(null, e.stack)[0].toString().includes('chai/lib'))
     }
 
-    Error.prepareStackTrace = orig;
-  });
-
-});
+    Error.prepareStackTrace = orig
+  })
+})
